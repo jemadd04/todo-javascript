533f1e55a785f8fe0cdd7004122ebaeb120293f2
James Maddox <james.maddox07@gmail.com>
2020-05-25 20:39:14 -0400

    app completed, added local storage func

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const todoInput = document.querySelector('.todo-input');
 const todoBtn = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo')
 
 // Functions
 function addTodo(e) {
@@ -14,6 +15,8 @@ function addTodo(e) {
     newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item')
     todoDiv.appendChild(newTodo);
+    // Save todo to LS
+    saveToLocal(todoInput.value);
     // Check mark - complete button
     const completedBtn = document.createElement('button');
     completedBtn.innerHTML = '<i class="fas fa-check"></i>';
@@ -26,8 +29,110 @@ function addTodo(e) {
     todoDiv.appendChild(trashBtn)
 
     todoList.appendChild(todoDiv)
+    // Clear todo input value
     todoInput.value = '';
+}
+
+
+function deleteComplete(e) {
+    const item = e.target;
+    if(item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        // Animation
+        todo.classList.add('fall');
+        removeLocalTodos(todo);
+        todo.addEventListener('transitionend', function() {
+            todo.remove();
+        })
+    }
+
+    if(item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(todo => {
+        switch(e.target.value) {
+            case "all":
+                todo.style.display = 'flex'
+                break;
+            case "completed":
+                if(todo.classList.contains('completed')) {
+                    todo.style.display = 'flex'
+                } else {
+                    todo.style.display = 'none'
+                }
+                break;
+            case "uncompleted":
+                if(!(todo.classList.contains('completed'))) {
+                    todo.style.display = 'flex'
+                } else {
+                    todo.style.display = 'none'
+                }
+                break;
+        }
+    })
+}
+
+function saveToLocal(todo) {
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+function getTodos() {
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    todos.forEach(function(todo) {
+        // Create todo div
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo');
+        // Create li
+        const newTodo = document.createElement('li');
+        newTodo.innerText = todo;
+        newTodo.classList.add('todo-item')
+        todoDiv.appendChild(newTodo);
+        // Check mark - complete button
+        const completedBtn = document.createElement('button');
+        completedBtn.innerHTML = '<i class="fas fa-check"></i>';
+        completedBtn.classList.add('complete-btn');
+        todoDiv.appendChild(completedBtn)
+        // Trash button
+        const trashBtn = document.createElement('button');
+        trashBtn.innerHTML = '<i class="fas fa-trash"></i>';
+        trashBtn.classList.add('trash-btn');
+        todoDiv.appendChild(trashBtn)
+
+        todoList.appendChild(todoDiv)
+    })
+}
+
+function removeLocalTodos(todo) {
+    let todos;
+    if(localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'))
+    }
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
 
 // Event Listeners
 todoBtn.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteComplete);
+filterOption.addEventListener('click', filterTodo)
+document.addEventListener('DOMContentLoaded', getTodos)
